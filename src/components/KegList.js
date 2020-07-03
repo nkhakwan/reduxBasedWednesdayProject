20ec68c3f3c43efbcdae693c0c50e63fe96abc58
nkhakwan <post_khan@yahoo.com>
2020-07-03 14:52:08 -0700

    some corrections to kegList but mostly my import for app.css for styling. app.css was copied and some changes done to it to adapt to taproom

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -11,15 +11,13 @@ export default function KegList(props) {
             <img src={keg.img} alt={keg.desc} />
             <div>
               <h3>{keg.name}</h3> 
-              <p className="desc"><strong>Description:</strong><br /> {keg.desc}</p>
+              <p className="brand"><strong>Brand:</strong><br /> {keg.brand}</p>
               <h4> <strong>Cost of Pint:</strong> ${keg.price}</h4>
               <h5><strong>Stock:</strong> {keg.quantity > 0 ? keg.quantity : <strong>Out of stock</strong>}</h5>
+              <h5><strong>Alcohol Contents:</strong> {keg.content}</h5>
             </div>
             <div className="buttons">
               {keg.quantity > 0 ? <button className="buy" onClick={() => buy(keg.id)}>buy</button> : <button className="buy" onClick={() => buy(keg.id)} disabled>buy</button>}
-              
-              
-              
             </div>
           </div>
         )
